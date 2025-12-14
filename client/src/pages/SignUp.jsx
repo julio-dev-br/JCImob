48e3b911e2dec9fe31ecfa4f37e24c8e06249352
Julio Valente <juliovalente2021@gmail.com>
2025-12-13 23:04:58 -0300

    Criação da pagina registrar ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Criar conta</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" id='username' placeholder='Usuário' className='p-3 rounded-lg bg-white outline-0' />
+        <input type="text" id='email' placeholder='Email' className='p-3 rounded-lg bg-white outline-0' />
+        <input type="text" id='password' placeholder='Senha' className='p-3 rounded-lg bg-white outline-0' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 cursor-pointer'>Registrar</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Já possui uma conta?</p>
+        <Link to={"/entrar"}>
+          <span className='text-blue-700'>Entrar</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
