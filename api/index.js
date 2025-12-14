d721e8e655cb6c8215a799aba743f7cfba06f88b
Julio Valente <juliovalente2021@gmail.com>
2025-12-13 21:20:23 -0300

    criação do teste api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import userRouter from './routes/user.route.js';
+
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to mongoDB');
 }).catch((err) => {
@@ -14,3 +16,5 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use("/api/user", userRouter);
