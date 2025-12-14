3955547c94b673c010be3c8d353bb63a4617c907
Julio Valente <juliovalente2021@gmail.com>
2025-12-13 22:10:27 -0300

    criação signup api rota

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
+dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to mongoDB');
@@ -13,8 +14,11 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
